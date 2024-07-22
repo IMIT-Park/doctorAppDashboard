6cc7e372f746dbe5eb2f6f5454ea7c9c6482abb8
jimlatantony <jimlatantony222@gmail.com>
2024-07-22 12:52:28 +0530

    booking cancel completed

--- src/panels/clinic/profile/CancelReschedule.jsx
+++ src/panels/clinic/profile/CancelReschedule.jsx
@@ -8,74 +8,84 @@ import { formatTime } from "../../../utils/formatTime";
 import { formatDate, reverseformatDate } from "../../../utils/formatDate";
 import { showMessage } from "../../../utils/showMessage";
 import IconLoader from "../../../components/Icon/IconLoader";
-import Flatpickr from "react-flatpickr";
 import "flatpickr/dist/flatpickr.css";
 
-const RescheduleModal = ({
-  addRescheduleModal,
-  closeAddRescheduleModal,
-  fetchAppointments,
+const CancelReschedule = ({
+  cancelRescheduleModal,
+  closeCancelRescheduleModal,
   bookingId,
+  selectedDoctorId,
+  fetchAppointments,
+  cancelAll,
+  clinicId,
+  selectedDate,
 }) => {
-  const [selectedDate, setSelectedDate] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const handleReschedule = async (event) => {
+  const handleCancelReschedule = async (event) => {
     event.preventDefault();
-    console.log("Selected Date:", selectedDate);
-    console.log("Booking ID:", bookingId);
-
-    if (!selectedDate) {
-      showMessage("Please select a date.", "error");
-      return;
-    }
-
     setLoading(true);
 
     try {
       const response = await NetworkHandler.makePostRequest(
-        `/v1/consultation/Reschedule/${bookingId}`,
+        `/v1/consultation/cancel/${bookingId}`
+      );
+
+      if (response.status === 200) {
+        showMessage("Appointment canceled successfully!", "success");
+        closeCancelRescheduleModal();
+        fetchAppointments();
+      } else {
+        showMessage(
+          "Failed to cancel the appointment. Please try again.",
+          "error"
+        );
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      showMessage("An error occurred. Please try again.", "error");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const CancelAllAppoinments = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await NetworkHandler.makePostRequest(
+        `/v1/consultation/cancelBooking/${selectedDoctorId}`,
         {
-          new_schedule_date: reverseformatDate(selectedDate),
+          clinic_id: clinicId,
+          schedule_date: reverseformatDate(selectedDate),
         }
       );
 
-      console.log("Response:", response);
-
       if (response.status === 200) {
-        showMessage("Rescheduled successfully!", "success");
-        closeAddRescheduleModal();
+        showMessage("All Appointments canceled successfully!", "success");
+        closeCancelRescheduleModal();
         fetchAppointments();
       } else {
-        showMessage("Failed to reschedule. Please try again.", "error");
+        showMessage(
+          "Failed to cancel all appointments. Please try again.",
+          "error"
+        );
       }
     } catch (error) {
-      console.log(error);
-      if (error.response && error.response.status === 404) {
-        showMessage(
-          `Doctor is on leave on ${formatDate(selectedDate)}`,
-          "warning"
-        );
-      } else if (error.response && error.response.status === 403) {
-        showMessage(
-          `Pick the date that corresponds to the day of the week`,
-          "warning"
-        );
-      } else {
-        showMessage("An error occurred. Please try again.", "error");
-      }
+      showMessage("An error occurred. Please try again.", "error");
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <Transition appear show={addRescheduleModal} as={Fragment}>
+    <Transition appear show={cancelRescheduleModal} as={Fragment}>
       <Dialog
         as="div"
-        open={addRescheduleModal}
+        open={cancelRescheduleModal}
         onClose={() => {
-          closeAddRescheduleModal();
+          closeCancelRescheduleModal();
         }}
         className="relative z-[51]"
       >
@@ -106,7 +116,7 @@ const RescheduleModal = ({
                 <button
                   type="button"
                   onClick={() => {
-                    closeAddRescheduleModal();
+                    closeCancelRescheduleModal();
                   }}
                   className="absolute top-4 ltr:right-4 rtl:left-4 text-gray-400 hover:text-gray-800 dark:hover:text-gray-600 outline-none"
                 >
@@ -116,29 +126,28 @@ const RescheduleModal = ({
                 <div className="p-5 ">
                   <div className="panel">
                     <h1 className="text-center font-bold text-2xl text-black m-8 dark:text-[#fbfbfb]">
-                      Select Date
+                      {cancelAll
+                        ? "Cancel All Appoinments"
+                        : "Cancel Appoinment"}
                     </h1>
-                    <form onSubmit={handleReschedule}>
-                      <div className="flex flex-wrap justify-center mb-6 space-x-4">
-                        <div className="mb-5">
-                          <Flatpickr
-                            value={selectedDate}
-                            options={{
-                              dateFormat: "Y-m-d",
-                              position: "auto left",
-                              inline: true,
-                            }}
-                            className="form-input"
-                            onChange={(date) => setSelectedDate(date)}
-                          />
-                        </div>
-                      </div>
+                    <h2 className="text-center font-bold text-md text-black m-8 dark:text-[#fbfbfb]">
+                      {cancelAll
+                        ? "Are you sure want to cancel all appointments ?"
+                        : "Are you sure want to cancel this appointment ?"}
+                    </h2>
+                    <form
+                      onSubmit={
+                        cancelAll
+                          ? CancelAllAppoinments
+                          : handleCancelReschedule
+                      }
+                    >
                       <div className="flex justify-center items-center mt-8">
                         <button
                           type="button"
                           className="btn btn-outline-danger gap-2"
                           onClick={() => {
-                            closeAddRescheduleModal();
+                            closeCancelRescheduleModal();
                           }}
                         >
                           Cancel
@@ -166,4 +175,4 @@ const RescheduleModal = ({
   );
 };
 
-export default RescheduleModal;
+export default CancelReschedule;
